b826457df63af79812afc19cd188af732559ff4f
Husain Mistry <husain.mlphd@gmail.com>
2026-07-23 23:16:08 +0530

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.mysterybox.deduceit"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     manifestPlaceholders["adMobAppId"] = testAdMobAppId
     buildConfigField("String", "ADMOB_APP_ID", "\"$testAdMobAppId\"")
